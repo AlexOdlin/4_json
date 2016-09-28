e1ff52a88751b4eaf21a3ce2e919cb0fb26b4bc4
AlexOdlin <rockguitarist94alex@gmail.com>
2016-09-29 00:18:11 +0300

    done

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,16 @@
 import json
 
+filepath = input('Введите путь к файлу: ')
 
 def load_data(filepath):
-    pass
-
+    with open (filepath, 'r', encoding='utf-8') as file_handler:
+        return json.load(file_handler)
+        
+data = load_data(filepath)
 
 def pretty_print_json(data):
-    pass
+    for elements in data:
+        print((json.dumps(elements, indent=4, ensure_ascii=False)))
+    return(data)
 
-
-if __name__ == '__main__':
-    pass
+print(pretty_print_json(data))
