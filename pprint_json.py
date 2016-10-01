2e6dfcf1b430bacc0f408a6e6bc73452a5c6dbff
AlexOdlin <rockguitarist94alex@gmail.com>
2016-10-01 21:02:43 +0300

    Исправление
    Исправлено замечание косательно __name__ == '__main__', а также
    добавлена проверка на некорректный ввод пути

--- pprint_json.py
+++ pprint_json.py
@@ -1,16 +1,22 @@
 import json
+import os
+from pprint import pprint
+
 
 filepath = 'Alco.json'
 
+
 def load_data(filepath):
+    if not os.path.exists(filepath):
+        return None
     with open (filepath, 'r', encoding='utf-8') as file_handler:
         return json.load(file_handler)
-        
-data = load_data(filepath)
 
-def pretty_print_json(data):
-        print((json.dumps(data, indent=4, ensure_ascii=False)))
-        return(data)
+
+def pretty_print_json(filepath):
+        return pprint(data)
+
 
 if __name__ == '__main__':
-    print(pretty_print_json(data))
+    data = load_data(input('Путь к файлу: '))
+    pretty_print_json(data)
